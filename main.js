08f9121da2e4238754f30b2d7aeb2a75047587b4
Surya K <suryak93813040@gmail.com>
2026-08-06 09:29:41 +0530

    fix(ci): fix github workflow paths and configure cross-platform build targets

--- main.js
+++ main.js
@@ -74,4 +74,18 @@ ipcMain.handle('get-metadata', async (e, filePath) => {
   }
 });
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
